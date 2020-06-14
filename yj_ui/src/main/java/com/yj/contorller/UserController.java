a1eb08afc82e36350010bdeff817c8b0da9a971c
17924529 <17924529@qq.com>
2020-06-14 17:42:18 +0800

    1:整合shiro权限认证 2:实现基本的登录认证 3:封装JSON->pojo pojo->json的方法类

--- yj_ui/src/main/java/com/yj/contorller/UserController.java
+++ yj_ui/src/main/java/com/yj/contorller/UserController.java
@@ -3,12 +3,16 @@ package com.yj.contorller;
 import com.yj.dto.User;
 import com.yj.feign.IUserServiceFeign;
 import com.yj.utils.JsonUtil;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class UserController {
@@ -19,7 +23,7 @@ public class UserController {
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("msg","欢迎光临");
-        return "login";
+        return "index";
     }
 
     @RequestMapping("/index")
@@ -27,17 +31,39 @@ public class UserController {
         return "index";
     }
 
+    @RequestMapping("/toLogin")
+    public String toLogin(){
+        return "login";
+    }
+
+
     @RequestMapping("/login")
-    public String login(String username,Model model){
-        String user = userServiceFeign.getUserInfo(username);
-        User userobj = JsonUtil.getObject(user,User.class);
-        System.out.println(user);
-        if (StringUtils.isEmpty(user) || user.equals("")){
+    public String login(String username,String password,Model model){
+        if(username == null){
+            model.addAttribute("msg","用户名不能为空");
+            return "login";
+        }
+
+        if(password == null){
+            model.addAttribute("msg","用户密码不能为空");
+            return "login";
+        }
+        /**
+         * 认证
+         */
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+        try{
+            subject.login(token);
+            model.addAttribute("msg","欢迎光临");
+            return "redirect:/index";
+        }catch (UnknownAccountException e){
             model.addAttribute("msg","用户名不存在");
             return "login";
-        }else{
-            return "redirect:/index";
+        }catch (IncorrectCredentialsException e){
+            model.addAttribute("msg","密码错误");
+            return "login";
         }
-        //return "redirect:/index";
+
     }
 }
---
+++ yj_ui/src/main/java/com/yj/shiro/UserRealm.java
@@ -0,0 +1,35 @@
+package com.yj.shiro;
+
+import com.yj.dto.User;
+import com.yj.feign.IUserServiceFeign;
+import com.yj.utils.JsonUtil;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserRealm extends AuthorizingRealm {
+    @Autowired
+    private IUserServiceFeign userServiceFeign;
+    private static final Logger logger = LoggerFactory.getLogger(UserRealm.class);
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        logger.info("-----进入授权逻辑------");
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        logger.info("-----进入验证逻辑------");
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+        String userInfo = userServiceFeign.getUserInfo(token.getUsername());
+        if (userInfo == null){
+            return null;
+        }
+        return new SimpleAuthenticationInfo("",
+                JsonUtil.getObject(userInfo,User.class).getPassword(),"");
+    }
+}
