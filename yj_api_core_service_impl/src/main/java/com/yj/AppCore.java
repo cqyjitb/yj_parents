631c47dbf14037583a168bea8a80dab8d584bb57
17924529 <17924529@qq.com>
2020-06-15 22:30:08 +0800

    更换注册中心 nacos 1.4

--- yj_api_core_service_impl/src/main/java/com/yj/AppCore.java
+++ yj_api_core_service_impl/src/main/java/com/yj/AppCore.java
@@ -4,13 +4,15 @@ import com.yj.db.config.db_core.DbCoreConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableScheduling
 @EnableConfigurationProperties({DbCoreConfig.class})
 public class AppCore {
--- yj_api_member_service_impl/src/main/java/com/yj/AppMember.java
+++ yj_api_member_service_impl/src/main/java/com/yj/AppMember.java
@@ -5,13 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableScheduling
 @EnableConfigurationProperties({DbCjConfig.class})
 public class AppMember {
