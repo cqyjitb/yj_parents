715d8d3d21b51ddf10028e2c63f29faa33d1e8a1
17924529 <17924529@qq.com>
2020-06-16 23:58:36 +0800

    集成Spring cloud gateway

--- yj_api_core_service_impl/src/main/java/com/yj/core/user/controller/UserController.java
+++ yj_api_core_service_impl/src/main/java/com/yj/core/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.yj.api.service.IUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    @Value("${server.port}")
+    private String port;
     @Autowired
     private IUserService userService;
     @RequestMapping("/getUserInfo")
     public String getUserInfo(@RequestBody String username){
         logger.info("日志测试！！！！！！！！");
         return userService.getUserInfo(username);
+    }
+
+    @RequestMapping("/test/1")
+    private String test(){
+
+        return "yj_core_api/test:"+port;
+    }
+
+    @RequestMapping("/test2/1")
+    private String test2(){
+        return "yj_core_api/test";
     }
 }
