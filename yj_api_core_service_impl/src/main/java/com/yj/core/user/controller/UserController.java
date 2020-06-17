3ea27a1b0367248ae3affe3668797a4354eef0b0
idmurder <17924529@qq.com>
2020-06-17 17:43:34 +0800

    集成 swagger2

--- yj_api_core_service_impl/src/main/java/com/yj/core/user/controller/UserController.java
+++ yj_api_core_service_impl/src/main/java/com/yj/core/user/controller/UserController.java
@@ -1,35 +1,45 @@
 package com.yj.core.user.controller;
 
 import com.yj.api.service.IUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api("用户接口")
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @Value("${server.port}")
     private String port;
     @Autowired
     private IUserService userService;
-    @RequestMapping("/getUserInfo")
+
+    @ApiOperation(value = "用户获取", notes = "根据用户名获取用户信息")
+    @ApiImplicitParam(name = "username",value = "用户名",required = true,paramType = "body",example = "tom")
+    @RequestMapping(value = "/getUserInfo",method = {RequestMethod.POST})
     public String getUserInfo(@RequestBody String username){
-        logger.info("日志测试！！！！！！！！");
-        return userService.getUserInfo(username);
+        logger.info("username:"+username);
+        String userinfo = "";
+        userinfo = userService.getUserInfo(username);
+        return userinfo;
     }
 
-    @RequestMapping("/test/1")
-    private String test(){
 
-        return "yj_core_api/test:"+port;
+    @ApiOperation(value = "计算+", notes = "加法")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "a", paramType = "path", value = "数字a", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "b", paramType = "path", value = "数字b", required = true, dataType = "Long")
+    })
+    @RequestMapping(value = "/test/{a}/{b}",method = {RequestMethod.GET})
+    public Integer get(@PathVariable Integer a, @PathVariable Integer b) {
+        return a + b;
     }
 
-    @RequestMapping("/test2/1")
-    private String test2(){
-        return "yj_core_api/test";
-    }
+
 }
